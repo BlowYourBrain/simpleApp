8f087f6c91f7e41988c8e6138022518da73342a0
BlowYourBrain <fetel@yandex.ru>
2018-09-20 17:56:07 +0300

    Реализовал FullSizeImageActivity. В принципе на этом всё.

--- app/src/main/java/com/company/regular/simpleapp/activity/FullSizeImageActivity.java
+++ app/src/main/java/com/company/regular/simpleapp/activity/FullSizeImageActivity.java
@@ -3,14 +3,19 @@ package com.company.regular.simpleapp.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.company.regular.simpleapp.R;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
 
 public class FullSizeImageActivity extends AppCompatActivity {
 
 	private static final String IMAGE_URL_KEY = "imageurlkey";
+	private ImageView mImageView;
 
 
 	public static void startActivity(Context context, String imageUrl) {
@@ -24,5 +29,23 @@ public class FullSizeImageActivity extends AppCompatActivity {
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_full_size_image);
+		mImageView = findViewById(R.id.image);
+
+		Bundle bundle = getIntent().getExtras();
+		if (bundle != null) {
+			String imageUrl = bundle.getString(IMAGE_URL_KEY);
+			setupImageView(imageUrl, mImageView);
+		}
+	}
+
+
+	private void setupImageView(String imageUrl, @NonNull ImageView imageView) {
+		if (imageUrl != null) {
+			Picasso.get()
+					.load(imageUrl)
+					.networkPolicy(NetworkPolicy.OFFLINE)
+					.placeholder(R.drawable.ic_baseline_photo_24px)
+					.into(imageView);
+		}
 	}
 }
